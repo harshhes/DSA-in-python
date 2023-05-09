f252525b2607f55184c763deadf9f0d02db3085f
HarsH <harshgolu82@gmail.com>
2023-05-09 12:09:47 +0530

    Time: 132 ms (75.93%), Space: 18.9 MB (14.63%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        unique_results = Counter(nums)
-        for key, value in unique_results.items():
-            if value == 1:
-                return key
+        return reduce(lambda total, el: total ^ el, nums)
+        # unique_results = Counter(nums)
+        # for key, value in unique_results.items():
+        #     if value == 1:
+        #         return key
