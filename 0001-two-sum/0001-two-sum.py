5cf4db37793d8f53bc54bc083929958e7c93074f
HarsH <harshgolu82@gmail.com>
2023-11-02 17:52:01 +0530

    Time: 64 ms (69.44%), Space: 17.5 MB (38.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,11 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         mapDict = {}
+        
         for i,n in enumerate(nums):
             diff = target - n
+            
             if diff in mapDict:
                 return [mapDict[diff], i]
+            
             mapDict[n] = i
