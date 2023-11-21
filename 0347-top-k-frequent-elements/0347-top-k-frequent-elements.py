85ba9904e298933081bd3beaf6beb16149175de2
HarsH <harshgolu82@gmail.com>
2023-11-21 18:33:04 +0530

    Time: 97 ms (67.28%), Space: 22 MB (5.58%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        num_dict = {}
-    
-        for i in nums:
-            if i in num_dict:
-                num_dict[i] += 1
-
-            else:
-                num_dict[i] = 1
-
-        # sorting in desc order based on the values of num_dict specified through key 
-        sorted_num_dict = sorted(num_dict.items(), key=lambda x:x[1], reverse=True)
+        count = {}
+        freq = [ [] for i in range(len(nums)+1)]
         
-        #sorted_num_dict[:k] -> taking k most frequent elements           
-        
-        return [ item[0] for item in sorted_num_dict[:k] ]
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+            
+        for n,c in count.items():
+            freq[c].append(n)
+            
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
