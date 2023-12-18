e607ec2d3d836450cc61d7a85d243dfa2329a38f
HarsH <harshgolu82@gmail.com>
2023-12-18 18:10:01 +0530

    Time: 1065 ms (63.33%), Space: 20.4 MB (78.60%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,42 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        
-        n, p, z = [], [], []
-        
-        for num in nums:
-            if num < 0:
-                n.append(num)
-            elif num > 0:
-                p.append(num)
+        ans = []
+        nums.sort()
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
                 
-            else:
-                z.append(num)
-                
-        N = set(n)
-        P = set(p)
-        
-        # (1,0,-1) = 0
-        if z:
-            for num in P:
-                if -1*num in N:
-                    res.add((-1*num, 0, num))
+            l, r = i + 1, len(nums) - 1
+            while l<r:
+                threeSum = nums[i] + nums[l] + nums[r]
+                if threeSum < 0:
+                    l += 1
                     
-        if len(z) >= 3:
-            res.add((0,0,0))
-            
-        # (-3,-2) , if 5 exists in P    
-        for i in range(len(n)):
-            for j in range(i+1, len(n)):
-                target = -1*(n[i]+n[j])
-                if target in P:
-                    res.add(tuple(sorted([n[i], n[j],target])))
+                elif threeSum > 0:
+                    r -= 1
                     
-        # (3,2) , if -5 exists in N 
-        for i in range(len(p)):
-            for j in range(i+1, len(p)):
-                target = -1*(p[i]+p[j])
-                if target in N:
-                    res.add(tuple(sorted([p[i], p[j], target])))
-        return res
+                else:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l<r:
+                        l += 1
+                        
+        return ans
                     
